b82e569e96ce3a7152bd8603d3ec5357db5cf82e
ZykoWen <2049973245@qq.com>
2024-11-23 15:49:18 +0800

    第二章代码

---
+++ os_chapter2/os/src/lang_items.rs
@@ -0,0 +1,17 @@
+use crate::sbi::shutdown;
+use core::panic::PanicInfo;//在核心库中得以保留
+#[panic_handler]
+fn panic(info:&PanicInfo) -> !{
+  if let Some(location) = info.location() {
+    println!(
+              "Panicked at {}:{} {}",
+              location.file(),
+              location.line(),
+              info.message()  
+              //message函数的返回值为PanicMessage，PanicMessage 实现了 Debug 或 Display，所以不再直接提供解包方法，例如 unwrap
+            );      
+  }else{
+    println!("Panicked: {}",info.message());
+  }
+  shutdown(true);
+}
---
+++ os_chapter2/os/src/sbi.rs
@@ -0,0 +1,16 @@
+//将内核与 RustSBI 通信的相关功能实现在子模块 sbi 中
+//输出字符串功能
+pub fn console_putchar(c: usize){
+  #[allow(deprecated)]
+  sbi_rt::legacy::console_putchar(c);
+}
+//关机功能
+pub fn shutdown(failure: bool) -> ! { //参数 failure 表示系统是否正常退出
+  use sbi_rt::{system_reset,NoReason,Shutdown,SystemFailure};
+  if !failure {
+    system_reset(Shutdown,NoReason);
+  }else{
+    system_reset(Shutdown, SystemFailure);
+  }
+  unreachable!();
+}
